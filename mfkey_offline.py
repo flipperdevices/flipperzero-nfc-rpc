e977845b18043ae5656f0bbd2ac87c52fc9b3054
gornekich <n.gorbadey@gmail.com>
2022-08-23 21:08:02 +0300

    add mfkey32 attack

--- mfkey_offline.py
+++ mfkey_offline.py
@@ -1,39 +1,106 @@
 #!/usr/bin/env python3
 
+import os
 import sys
-import serial
+import pprint
+import datetime
+import subprocess
 
-from flipperzero_protobuf_py.flipper_protobuf import ProtoFlipper
-from flipperzero_protobuf_py.cli_helpers import *
+from google.protobuf.json_format import MessageToDict
+from flipperzero_protobuf.flipper_proto import FlipperProto
+from flipperzero_protobuf.cli_helpers import *
+from flipperzero_protobuf.flipper_base import FlipperProtoException
+
+flipper_params_file_path = '/ext/nfc/detect_reader.log'
+flipper_user_dict_path = '/ext/nfc/assets/mf_classic_dict_user.nfc'
+
+
+def mfkey32v2(params):
+    mfkey_args = (params[2:])
+    args = ["flipper-nfc/mfkey"]
+    args.extend(mfkey_args)
+    result = subprocess.run(
+        args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='ascii')
+
+    if result.returncode == 0:
+        return result.stdout
+    else:
+        return None
 
 
 def main():
-    # open serial port
-    flipper = serial.Serial(sys.argv[1], timeout=1)
-    flipper.baudrate = 230400
-    flipper.flushOutput()
-    flipper.flushInput()
 
-    # disable timeout
-    flipper.timeout = None
+    proto = FlipperProto()
 
-    # wait for prompt
-    flipper.read_until(b'>: ')
+    print(f"Read flipper log file from {flipper_params_file_path}")
+    try:
+        log_file = proto.rpc_read(flipper_params_file_path).decode("utf-8")
+    except FlipperProtoException:
+        print("No new logs available")
+        return
 
-    # send command and skip answer
-    flipper.write(b"start_rpc_session\r")
-    flipper.read_until(b'\n')
+    reader_logs = log_file.splitlines()
+    print(f"\nFound {len(reader_logs)} params")
 
-    # construct protobuf worker
-    proto = ProtoFlipper(flipper)
+    print(f"\nStarting mfkey32v2 attack")
+    keys_found = set()
+    for line in reader_logs:
+        params = line.split(' ')[1::2]
+        print(f"\nProcessing sector {params[0]} key {params[1]}")
+        key = mfkey32v2(params)
+        if key is not None:
+            print(f"Retrived key {key}")
+            keys_found.add(key)
+        else:
+            print("Failed to retrieve key")
 
-    print("Ping result: ")
-    print_hex(proto.cmd_system_ping())
+    print(f"\nFound {len(keys_found)} unique keys:")
+    for key in keys_found:
+        print(key)
 
-    proto.cmd_gui_send_input("SHORT UP")
+    print(f"\nLoading user dictionary from {flipper_user_dict_path}")
 
-    print("Screen capture result: ")
-    print_screen(proto.cmd_gui_snapshot_screen())
+    try:
+        user_dict = proto.rpc_read(flipper_user_dict_path).decode("utf-8")
+    except FlipperProtoException:
+        print("No user dictionary found")
+        user_dict = ''
+
+    user_keys = set(user_dict.splitlines())
+    print(f"User dict contains {len(user_keys)} keys:")
+    for key in user_keys:
+        print(key)
+
+    new_user_keys = user_keys.copy()
+    for key in keys_found:
+        new_user_keys.add(key)
+
+    keys_added = len(new_user_keys) - len(user_keys)
+    if keys_added == 0:
+        print("No unique keys found")
+    else:
+        new_keys = new_user_keys - user_keys
+        print(f"\nFound {len(new_keys)} new keys:")
+        for key in new_keys:
+            print(key)
+
+        print("\nUpdating user dictionary")
+        new_user_dict = '\n'.join(key for key in new_user_keys)
+
+        try:
+            proto.rpc_write(
+                flipper_user_dict_path, new_user_dict.encode("utf-8"))
+        except FlipperProtoException:
+            print("Failed to update user dictionary")
+            return
+        
+        print("\nDeleting reader log file")
+
+        try:
+            proto.rpc_delete(flipper_params_file_path)
+        except FlipperProtoException:
+            print("Failed to delete reader log file")
+            return
 
 
 if __name__ == '__main__':
