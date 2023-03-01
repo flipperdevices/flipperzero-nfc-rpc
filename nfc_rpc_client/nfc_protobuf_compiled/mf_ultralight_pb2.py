8423220fd3fe8bf1766589f6ce7c0cbd917de1f4
gornekich <n.gorbadey@gmail.com>
2023-03-01 14:45:45 +0400

    new rpc messages

---
+++ nfc_rpc_client/nfc_protobuf_compiled/mf_ultralight_pb2.py
@@ -0,0 +1,16 @@
+"""Generated protocol buffer code."""
+from google.protobuf.internal import builder as _builder
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import symbol_database as _symbol_database
+_sym_db = _symbol_database.Default()
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13mf_ultralight.proto\x12\x0fPB_MfUltralight"\x1f\n\x0fReadPageRequest\x12\x0c\n\x04page\x18\x01 \x01(\r".\n\x10ReadPageResponse\x12\x0c\n\x04page\x18\x01 \x01(\r\x12\x0c\n\x04data\x18\x02 \x01(\x0cB!\n\x1fcom.flipperdevices.nfc.protobufb\x06proto3')
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'mf_ultralight_pb2', globals())
+if _descriptor._USE_C_DESCRIPTORS == False:
+    DESCRIPTOR._options = None
+    DESCRIPTOR._serialized_options = b'\n\x1fcom.flipperdevices.nfc.protobuf'
+    _READPAGEREQUEST._serialized_start = 40
+    _READPAGEREQUEST._serialized_end = 71
+    _READPAGERESPONSE._serialized_start = 73
+    _READPAGERESPONSE._serialized_end = 119
---
+++ nfc_rpc_client/nfc_protobuf_compiled/nfca_pb2.py
@@ -0,0 +1,16 @@
+"""Generated protocol buffer code."""
+from google.protobuf.internal import builder as _builder
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import symbol_database as _symbol_database
+_sym_db = _symbol_database.Default()
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nnfca.proto\x12\x07PB_Nfca"\r\n\x0bReadRequest"G\n\x0cReadResponse\x12\x0b\n\x03uid\x18\x02 \x01(\x0c\x12\x0b\n\x03sak\x18\x03 \x01(\x0c\x12\x0c\n\x04atqa\x18\x04 \x01(\x0c\x12\x0f\n\x07uid_len\x18\x05 \x01(\rB!\n\x1fcom.flipperdevices.nfc.protobufb\x06proto3')
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'nfca_pb2', globals())
+if _descriptor._USE_C_DESCRIPTORS == False:
+    DESCRIPTOR._options = None
+    DESCRIPTOR._serialized_options = b'\n\x1fcom.flipperdevices.nfc.protobuf'
+    _READREQUEST._serialized_start = 23
+    _READREQUEST._serialized_end = 36
+    _READRESPONSE._serialized_start = 38
+    _READRESPONSE._serialized_end = 109
